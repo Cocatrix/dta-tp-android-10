acfcbb3fe5a16baefbd907eacf79a865c606062d
Cocatrix <cocatrix@laposte.net>
2017-10-13 15:59:51 +0200

    Enter, refresh, empty

--- app/src/main/java/fr/codevallee/formation/tp10app/CalculatorActivity.java
+++ app/src/main/java/fr/codevallee/formation/tp10app/CalculatorActivity.java
@@ -26,7 +26,7 @@ public class CalculatorActivity extends AppCompatActivity {
          * "0"+"3" = "03"
          * String(Integer("0"+"3") = "3"
          */
-        Integer totalPrintedNumber = Integer.parseInt(currentNumber.getText().toString() + number);
+        Float totalPrintedNumber = Float.parseFloat(currentNumber.getText().toString() + number);
         currentNumber.setText(totalPrintedNumber.toString());
         Log.d("STATE - ", number + " added to currentText");
     }
@@ -55,7 +55,9 @@ public class CalculatorActivity extends AppCompatActivity {
         // Empty TextView currentNumber
         TextView currentNumber = (TextView) findViewById(R.id.current_number);
         currentNumber.setText("");
-        // TODO - Empty the stack (in Java and in TextViews)
+        // Empty the stack in Java and TextViews
+        this.stack.empty();
+        this.refreshUIStack();
         Log.d("STATE - ", "Emptied");
     }
 
@@ -66,5 +68,35 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 
     public void click_enter(View view) {
+        TextView TextCurrentNumber = (TextView) findViewById(R.id.current_number);
+        String currentNumber = TextCurrentNumber.getText().toString();
+        if (currentNumber.equals("")&&currentNumber.equals(Float.parseFloat("0"))) {
+            this.stack.add(Float.parseFloat(currentNumber));
+            this.refreshUIStack();
+        }
+    }
+    
+    public void refreshUIStack() {
+        /**
+         * Rewrites in TextViews every value of the stack, up to the 4 first ones.
+         * If the size is less than 4, empty the rest.
+         */
+        // this.stack is regarded as up-to-date here
+        TextView stackNumber4 = (TextView) findViewById(R.id.stack_number_4);
+        TextView stackNumber3 = (TextView) findViewById(R.id.stack_number_3);
+        TextView stackNumber2 = (TextView) findViewById(R.id.stack_number_2);
+        TextView stackNumber1 = (TextView) findViewById(R.id.stack_number_1);
+        TextView[] stackNumbers = {stackNumber1,stackNumber2,stackNumber3,stackNumber4};
+        // We write 4 times maximum, or just the size of the stack if 4 or less elements
+        Integer numberWritings = ((this.stack.size()<4)?this.stack.size():4);
+        Integer iWritings,iDeletings;
+        for (iWritings=0;iWritings<numberWritings;iWritings++) {
+            stackNumbers[iWritings].setText(this.stack.elementAt(iWritings).toString());
+        }
+        // We empty the rest
+        for (iDeletings=iWritings;iDeletings<4;iDeletings++) {
+            stackNumbers[iWritings].setText("");
+        }
+
     }
 }
