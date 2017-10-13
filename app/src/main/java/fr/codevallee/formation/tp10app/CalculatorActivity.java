440f5b45e5e981de86bc2483db1533e32f1a18c4
Cocatrix <cocatrix@laposte.net>
2017-10-13 14:57:19 +0200

    Add clicked number (0 included) on current. Handle first zero

--- app/src/main/java/fr/codevallee/formation/tp10app/CalculatorActivity.java
+++ app/src/main/java/fr/codevallee/formation/tp10app/CalculatorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Stack;
 
@@ -14,15 +15,33 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-
         Log.d("STATE - ", "Begin");
     }
 
     public void click_1_to_9(View view) {
-
+        String number = (String) view.getTag();
+        Log.d("STATE - ", "Clicked on " + number);
+        TextView currentNumber = (TextView) findViewById(R.id.current_number);
+        /* Converting to Integer and then String allows to remove unwanted zeros. Indeed
+         * "0"+"3" = "03"
+         * String(Integer("0"+"3") = "3"
+         */
+        Integer totalPrintedNumber = Integer.parseInt(currentNumber.getText().toString() + number);
+        currentNumber.setText(totalPrintedNumber.toString());
+        Log.d("STATE - ", number + " added to currentText");
     }
 
     public void click_0(View view) {
+        String number = "0";
+        Log.d("STATE - ", "Clicked on " + number);
+        TextView currentNumber = (TextView) findViewById(R.id.current_number);
+        /* Converting to Integer and then String allows to remove unwanted zeros. Indeed
+         * "0"+"0" = "00"
+         * String(Integer("0"+"0") = "0"
+         */
+        Integer totalPrintedNumber = Integer.parseInt(currentNumber.getText().toString() + number);
+        currentNumber.setText(totalPrintedNumber.toString());
+        Log.d("STATE - ", number + " added to currentText");
     }
 
     public void click_plus(View view) {
@@ -44,6 +63,12 @@ public class CalculatorActivity extends AppCompatActivity {
     }
 
     public void click_clear(View view) {
+        String tag = (String) view.getTag();
+        Log.d("STATE - ", "Clicked on " + tag);
+        TextView currentNumber = (TextView) findViewById(R.id.current_number);
+        currentNumber.setText("");
+        // TODO - Empty the stack (in Java and in TextViews)
+        Log.d("STATE - ", "Emptied");
     }
 
     public void click_pop(View view) {
